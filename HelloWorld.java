ed7b7146f3cbe68e359d7d5639258fc91a63911b
nadiabrandl <64158822+nadiabrandl@users.noreply.github.com>
2021-03-11 23:40:39 -0500

    Revert "modification"
    This reverts commit 828ebf9c7dc67ef5c13aac349c7193aee9c65e75.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,9 @@
-package CSC_249;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello WOrld!!");
-		System.out.println("Program modified, but able to revert to original" + "version using GitHub Desktop.");
-
 	}
+
 }
